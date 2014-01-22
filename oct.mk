c5fa5fe52476474577e6d8bf67ce7651bfad8b47
Carl Windhausen <cjwindhausen@gmail.com>
2014-01-22 09:57:01 -0500

    Update and rename cm.mk to oct.mk

--- oct.mk
+++ oct.mk
@@ -1,13 +1,7 @@
 $(call inherit-product, device/samsung/d2spi/full_d2spi.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/cdma.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Oct stuff.
+$(call inherit-product, vendor/oct/config/cdma.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2spi \
@@ -15,6 +9,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="samsung/d2spi/d2spi:4.1.2/JZO54K/L710WWAMD5:user/release-keys" \
     PRIVATE_BUILD_DESC="d2spi-user 4.1.2 JZO54K L710WWAMD5 release-keys"
 
-PRODUCT_NAME := cm_d2spi
+PRODUCT_NAME := oct_d2spi
 PRODUCT_DEVICE := d2spi
 
