00baaedde430db8eb3be362045d8510272d72353
Carl Windhausen <cjwindhausen@gmail.com>
2014-01-30 08:25:48 -0500

    Update oct.mk

--- oct.mk
+++ oct.mk
@@ -1,7 +1,11 @@
 $(call inherit-product, device/samsung/d2spi/full_d2spi.mk)
 
-# Inherit some common Oct stuff.
+# Enhanced NFC
+$(call inherit-product, vendor/oct/config/nfc_enhanced.mk)
+
+# Inherit some common OctOS stuff.
 $(call inherit-product, vendor/oct/config/cdma.mk)
+$(call inherit-product, vendor/oct/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2spi \
